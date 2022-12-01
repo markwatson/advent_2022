8dfab73e0ec66c842b2262e38f881c04db525e2d
Mark Watson <git@markwatson.dev>
2022-12-01 12:51:20 -0700

    Refactored code a bit

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,55 +1,64 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
+use std::process::exit;
 
 fn main() {
+    let mut elves: Vec<Elf>;
+    if let Ok(lines) = read_lines("./data/day_1") {
+        elves = parse(lines);
+    } else {
+        println!("Error reading file");
+        exit(1);
+    }
+
+    // Find main elf
+    let top_elf = top_n_calories(&mut elves, 1);
+    println!("Elf with most calories has this many calories: {}", top_elf);
+
+    // Find top three elves
+    let top_three = top_n_calories(&mut elves, 3);
+    println!("Top three elves have this many calories: {}", top_three);
+}
+
+fn top_n_calories(elves: &mut Vec<Elf>, n: usize) -> i32 {
+    let mut top_n_calories: i32 = 0;
+    elves.sort_by(|a, b| b.calories.cmp(&a.calories));
+    for elf in elves.iter().take(n) {
+        top_n_calories += elf.calories;
+    }
+    return top_n_calories;
+}
+
+fn parse(lines: Lines<BufReader<File>>) -> Vec<Elf> {
     let mut elves: Vec<Elf> = Vec::new();
 
-    // Parse input
-    if let Ok(lines) = read_lines("./data/day_1") {
-        // Consumes the iterator, returns an (Optional) String
-        let mut items = Vec::new();
-        let mut counter: i32 = 1;
-        for line in lines {
-            if let Ok(item) = line {
-                if item.len() > 0 {
-                    items.push(item.parse::<i32>().unwrap());
-                } else {
-                    elves.push(Elf {
-                        id: counter,
-                        calories: items.iter().sum(),
-                        items: items.clone(),
-                    });
-                    items = Vec::new();
-                    counter += 1;
-                }
+    // Consumes the iterator, returns an (Optional) String
+    let mut items = Vec::new();
+    //let mut counter: i32 = 1;
+    for line in lines {
+        if let Ok(item) = line {
+            if item.len() > 0 {
+                items.push(item.parse::<i32>().unwrap());
+            } else {
+                elves.push(Elf {
+                    //id: counter,
+                    calories: items.iter().sum(),
+                    //items: items.clone(),
+                });
+                items = Vec::new();
+                //counter += 1;
             }
         }
     }
 
-    // Find main elf
-    let mut max: i32 = 0;
-    for elf in &elves {
-        if elf.calories > max {
-            max = elf.calories;
-        }
-    }
-    println!("Elf with most calories has this many calories: {}", max);
-
-    // Find top three elves
-    let mut top_3_calories: i32 = 0;
-    elves.sort_by(|a, b| b.calories.cmp(&a.calories));
-    for elf in elves.iter().take(3) {
-        println!("Elf {} has {} calories", elf.id, elf.calories);
-        top_3_calories += elf.calories;
-    }
-    println!("Top 3 elves have this many calories: {}", top_3_calories);
+    return elves;
 }
 
 struct Elf {
-    id: i32,
+    //id: i32,
     calories: i32,
-    items: Vec<i32>,
+    //items: Vec<i32>,
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
