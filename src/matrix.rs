fb44a424894435b3cc263f5b78d789e6270e56f2
Mark Watson <git@markwatson.dev>
2022-12-13 17:53:22 -0700

    Day 13: python works, rust does not. TODO: find out why

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,11 +2,12 @@ use std::{
     fmt::{Display, Formatter, Result},
     fs::read_to_string,
 };
-
+#[allow(dead_code)]
 pub struct Matrix<T: Copy + Display> {
     pub data: Vec<Vec<T>>,
 }
 
+#[allow(dead_code)]
 impl<T: Copy + Display> Display for Matrix<T> {
     fn fmt(&self, fmt: &mut Formatter) -> Result {
         let mut str = "";
@@ -23,6 +24,7 @@ impl<T: Copy + Display> Display for Matrix<T> {
     }
 }
 
+#[allow(dead_code)]
 impl<T: Copy + Display> Matrix<T> {
     pub fn new(data: Vec<Vec<T>>) -> Matrix<T> {
         Matrix { data }
@@ -93,22 +95,23 @@ impl<T: Copy + Display> Matrix<T> {
 
     pub fn neighbors_idx(&self, r: usize, c: usize) -> Vec<(usize, usize)> {
         let mut neighbors = vec![];
-        if let Some(n) = self.up(r, c) {
+        if let Some(_) = self.up(r, c) {
             neighbors.push((r - 1, c));
         }
-        if let Some(n) = self.down(r, c) {
+        if let Some(_) = self.down(r, c) {
             neighbors.push((r + 1, c));
         }
-        if let Some(n) = self.left(r, c) {
+        if let Some(_) = self.left(r, c) {
             neighbors.push((r, c - 1));
         }
-        if let Some(n) = self.right(r, c) {
+        if let Some(_) = self.right(r, c) {
             neighbors.push((r, c + 1));
         }
         neighbors
     }
 }
 
+#[allow(dead_code)]
 impl Matrix<char> {
     pub fn from_file(fname: &str) -> Matrix<char> {
         let input = read_to_string(fname).expect("Error reading file");
